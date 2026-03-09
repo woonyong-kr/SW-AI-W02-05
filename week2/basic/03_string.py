f3ed6001239762abc95308d7151aa567b61fdfad
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-09 11:44:43 +0900

    문자열 - 회문 판별

--- week2/basic/03_string.py
+++ week2/basic/03_string.py
@@ -34,16 +34,17 @@ def is_palindrome(s):
     Returns:
         bool: 회문이면 True, 아니면 False
     """
-    # TODO: 알파벳과 숫자만 남기고 소문자로 변환하세요
-    # 힌트: isalnum() 메서드와 lower() 메서드 사용
-    pass
-    
-    # TODO: 정제된 문자열이 회문인지 확인하세요
-    # 방법1: 문자열을 뒤집어서 비교 ([::-1] 사용)
-    # 방법2: 양 끝 인덱스를 이용한 투 포인터 방식
-    pass
-    
-    #return False
+
+    # 스트링 사용 - 리스트 보다는 메모리 효율적 (연결된 메모리 블록 사용)
+    context = "".join(c.lower() for c in s if c.isalnum())
+    # 리스트 사용 - 서로 떨어진 메모리 블록 사용
+    context = [c.lower() for c in s if c.isalnum()]
+    n = len(context)
+    for i in range(n):
+        if context[i] != context[n -1 - i]:
+            return False
+        
+    return True
 
 # 테스트 케이스
 if __name__ == "__main__":
