a639b79dcd933cd25bd14ecf69daebc1e55662d3
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-09 15:17:40 +0900

    정렬 - 버블 정렬 구현

--- week2/basic/08_bubble_sort.py
+++ week2/basic/08_bubble_sort.py
@@ -33,15 +33,11 @@ def bubble_sort(arr):
         정렬된 배열
     """
     n = len(arr)
-    
-    # TODO: 외부 반복문 - n-1번 반복
-    # 각 패스마다 가장 큰 원소가 끝으로 이동
-    ## TODO: 내부 반복문 - 인접한 원소 비교
-    ## 0부터 n-i-1까지 반복 (이미 정렬된 뒷부분 제외)
-    ## TODO: 인접한 두 원소 비교 및 교환
-    ## arr[j] > arr[j+1]이면 교환
-    ## 외부 반복문: n-1번 실행
-    pass
+
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
         
     return arr
 
@@ -57,16 +53,15 @@ def bubble_sort_optimized(arr):
     """
     n = len(arr)
     
-    for i in range(n):
-        swapped = False  # 교환 발생 여부
-        
-        # TODO: 내부 반복문과 교환 로직 구현
-        # 교환이 발생하면 swapped = True 설정        
-        pass
-        
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
 
-        # TODO: 교환이 없으면 이미 정렬된 것이므로 break
-        pass
+        if not swapped:
+            break
 
     return arr
 
