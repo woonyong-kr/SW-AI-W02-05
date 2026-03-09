9daa6e58f694852d46cd7d9b5020489c9342fa7d
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-10 01:24:31 +0900

    배열 - 평균은 넘겠지

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,21 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+import sys
+
+data = iter(map(int, sys.stdin.read().split()))
+
+c = next(data)
+
+for _ in range(c):
+    n = next(data)
+    scores = []
+    for _ in range(n):
+        scores.append(next(data))
+
+    average = sum(scores) / n
+    above_average_student_count = 0
+    for score in scores:
+        if score > average:
+            above_average_student_count += 1
+    print(f"{(above_average_student_count / n) * 100:.3f}%")
