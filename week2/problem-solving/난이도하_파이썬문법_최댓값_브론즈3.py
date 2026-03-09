3d2d0f51239df9a73412d400cecf1fa71b4188f0
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-09 18:38:28 +0900

    파이썬 문법 - 최댓값 (백준 브론즈3)

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,12 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+numbers = []
+for _ in range(9):
+    numbers.append(int(input()))
+    
+max_number = max(numbers)
+max_number_index = numbers.index(max_number) + 1
+
+print(max_number)
+print(max_number_index)
