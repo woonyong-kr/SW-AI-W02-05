67ef9fe030a32a81efafc74bb28d58ec4c7567ef
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-09 18:38:50 +0900

    정수론 - 최대공약수(GCD)와 최소공배수(LCM)

--- week2/basic/10_number_theory.py
+++ week2/basic/10_number_theory.py
@@ -34,10 +34,13 @@ def gcd(a, b):
     Returns:
         최대공약수
     """
-    # TODO: 유클리드 호제법 구현
-    # base case: b가 0이면 a 반환
-    # recursive를 이용 
-    pass
+    c = a % b
+
+    if c == 0:
+        return b
+
+    return gcd(b, c) 
+
 
 def gcd_iterative(a, b):
     """
@@ -49,9 +52,10 @@ def gcd_iterative(a, b):
     Returns:
         최대공약수
     """
-    # TODO: 반복문으로 구현
-    # b가 0이 될 때까지 반복
-    pass
+    while not b == 0:
+        a, b = b, a % b
+
+    return a
 
 def lcm(a, b):
     """
@@ -63,8 +67,8 @@ def lcm(a, b):
     Returns:
         최소공배수
     """
-    # TODO: LCM 계산
-    pass
+    return (a * b) // gcd(a, b)
+
 
 def extended_gcd(a, b):
     """
@@ -77,11 +81,16 @@ def extended_gcd(a, b):
     Returns:
         (gcd, x, y) 튜플
     """
-    # TODO: 확장 유클리드 호제법 구현
-    # base case: b가 0이면 (a, 1, 0) 반환    
-    # recursive case
-    # 역추적하며 x, y 계산
-    pass
+    if b == 0:
+        return (a, 1, 0)
+    
+    (gcd, x1, y1) = extended_gcd(b, a % b)
+    
+    x = y1
+    y = x1 - (a // b) * y1
+    
+    return (gcd, x, y)
+    
 
 def is_prime(n):
     """
@@ -93,11 +102,20 @@ def is_prime(n):
     Returns:
         소수이면 True, 아니면 False
     """
-    # TODO: 소수 판별 구현
-    # n이 2보다 작으면 False
-    # 2부터 sqrt(n)까지 나누어 떨어지는지 확인    
-    # 3부터 sqrt(n)까지 홀수만 확인
-    pass 
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    
+    sqrt_n = int(n**0.5)
+
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+        
+    return True
 
 # 테스트 케이스
 if __name__ == "__main__":
