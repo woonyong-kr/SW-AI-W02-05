38ebb5111a516c6a2de31b7d68a922b84f89275a
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-10 01:37:20 +0900

    문자열 - 문자열 반복

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,17 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+import sys
+
+data = iter(sys.stdin.read().split())
+
+c = int(next(data))
+
+result = ["" for _ in range(c)]
+for i in range(c):
+    r = int(next(data))
+    s = next(data)
+    result[i] = "".join(result[i] + char * r for char in s)
+
+for res in result:
+    print(res)
