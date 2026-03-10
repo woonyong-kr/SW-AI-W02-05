a7885d517cd3dda9dac790981513b890f34b7f57
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-10 10:04:55 +0900

    백트랙킹 - 조합 생성

--- week2/basic/06_backtracking.py
+++ week2/basic/06_backtracking.py
@@ -43,17 +43,18 @@ def combinations(n, k):
             start: 탐색을 시작할 숫자
             current_combination: 현재까지 선택한 숫자들
         """
-        # TODO: base case - k개를 모두 선택했으면 결과에 추가
-        pass
+
+        if len(current_combination) == k:
+            result.append(current_combination[:])
+            return
         
-        # TODO: start부터 n까지 숫자를 하나씩 시도
-        ## TODO: 백트랙킹 3단계 구현
-        ## 1. 선택(Choose)
-        ## 2. 탐색(Explore)
-        ## 3. 취소(Unchoose)
-        pass
-    
+        for i in range(start, n + 1):
+            current_combination.append(i)
+            backtrack(i + 1, current_combination)
+            current_combination.pop()
+
     backtrack(1, [])
+
     return result
 
 def combinations_itertools_compare(n, k):
