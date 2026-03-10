d0003a3c8917395c4108cc731dbc2918667c0a98
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-10 12:45:52 +0900

    문자열 - IPv6

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,30 @@
 # 문자열 - IPv6 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/3107
+
+ipv6 = input()
+
+def zfill(padding, token):
+    return "0" * (padding - len(token)) + token
+
+parts = ipv6.split("::")
+
+full_address = []
+tokens = []
+if len(parts) == 1:
+    tokens = parts[0].split(":")
+else:
+    left = parts[0].split(":")
+    right = parts[1].split(":")
+
+    for address in left:
+        tokens.append(address)
+
+    for address in range(8 - (len(left) + len(right))):
+        tokens.append("0000")
+        
+    for address in right:
+        tokens.append(address)
+
+full_address = [zfill(4, token) for token in tokens]
+
+print(":".join(full_address))
