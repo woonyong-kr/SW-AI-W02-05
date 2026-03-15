3ee4ffafcdc4d938f7641d753aa7e28e715e9126
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-16 00:17:35 +0900

    해시 테이블 - 민균이의 비밀번호

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,35 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+import sys
+
+def solution_1():
+    data = sys.stdin.read().split()
+    if not data: return
+
+    n = int(data[0])
+    words = data[1:]
+
+    for i in range(n):
+        mid = len(words[i]) // 2
+        reversed_word = words[i][::-1]
+        for j in range(i, n):
+            if words[j] == reversed_word:
+                print(f"{len(words[i])} {words[i][mid]}")
+                return
+            
+def solution_2():
+    data = sys.stdin.read().split()
+    if not data: return
+
+    n = int(data[0])
+    words = set(data[1:])
+
+    for word in words:
+        if word[::-1] in words:
+            print(f"{len(word)} {word[len(word) // 2]}")
+            return
+
+if __name__ == "__main__":
+    #solution_1()
+    solution_2()
