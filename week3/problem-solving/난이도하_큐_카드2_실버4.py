7365c892bf159ce3eba824bff7d2cb5e429872c4
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-16 01:07:09 +0900

    큐 - 카드2

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,21 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+import sys
+from collections import deque
+
+def solution():
+    data = sys.stdin.read().split()
+    if not data: return
+
+    n = int(data[0])
+    cards = deque([i for i in range(1, n + 1)])
+
+    while len(cards) > 1:
+            cards.popleft()
+            cards.append(cards.popleft())
+
+    print(f"{cards[0]}")
+
+if __name__ == "__main__":
+    solution()
