e63483e3a57796ca5047be713d06a1f5460f0422
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-17 16:40:05 +0900

    스택 - 스택

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,38 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+import sys
+from collections import deque
+
+def solution():
+    data = sys.stdin.read().split()
+    if not data: 
+         return
+    
+    n = int(data[0])
+    commands = data[1:]
+    stack = []
+    result = []
+
+    ops = {
+        "pop": lambda: stack.pop() if stack else -1,
+        "size":lambda: len(stack),
+        "empty":lambda: 1 if not stack else 0,
+        "top":lambda: stack[-1] if stack else -1
+    }
+
+    curr = 0
+    while curr < len(commands):
+        cmd = commands[curr]
+
+        if cmd == "push":
+            stack.append(commands[curr + 1])
+            curr += 2
+        else:
+            result.append(str(ops[cmd]()))
+            curr += 1
+
+    sys.stdout.write("\n".join(result) + "\n")
+
+if __name__ == "__main__":
+    solution()
