f6cee2dc7d252fec3c3515b66e0c18d14d59cfe8
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-30 15:53:57 +0900

    refactor: week5 Team DP 문제 파일명 변경 (Extra → Team)

--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,30 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
+
+import sys
+
+def solution():
+    data = list(map(int, sys.stdin.buffer.read().split()))
+    t = data[0]
+    idx = 1
+
+    cases = []
+
+    for _ in range(t):
+        n = data[idx]
+        idx += 1
+
+        applicants = []
+        for _ in range(n):
+            doc = data[idx]
+            interview = data[idx + 1]
+            applicants.append((doc, interview))
+            idx += 2
+
+        cases.append(applicants)
+        
+    result = 0
+    
+    print(result)
+    
+solution()
