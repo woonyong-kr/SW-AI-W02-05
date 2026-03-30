74ebf77f8672a70dc36fa87abeb3d8a40a4cd394
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-30 22:12:13 +0900

    feat: 삼각형 최소 경로합 DP 풀이를 추가

--- week5/problem-solving/Team_DP_Triangle.py
+++ week5/problem-solving/Team_DP_Triangle.py
@@ -1,2 +1,9 @@
 # DP - Triangle
 # 문제 링크: https://leetcode.com/problems/triangle/description/?envType=study-plan-v2&envId=top-interview-150
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int: # type: ignore
+        for i in range(len(triangle)-2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+
+        return triangle[0][0]
