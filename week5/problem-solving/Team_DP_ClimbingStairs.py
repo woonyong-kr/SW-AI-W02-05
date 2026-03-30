50c557a119269f2b6611727a0c288f658c634c85
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-30 22:11:45 +0900

    feat: 계단 오르기 DP 풀이를 추가

--- week5/problem-solving/Team_DP_ClimbingStairs.py
+++ week5/problem-solving/Team_DP_ClimbingStairs.py
@@ -1,2 +1,17 @@
 # DP - Climbing Stairs
 # 문제 링크: https://leetcode.com/problems/climbing-stairs/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        a, b = 1, 2
+
+        for _ in range(3, n + 1):
+            a, b = b, a + b
+
+        return b
