d6fed9551fc5bdf73102bdc778eef4e6e4a954ce
choi woo-nyong <woonyong.kr@gmail.com>
2026-04-01 10:50:00 +0900

    feat: 신입사원 그리디 풀이를 완성

--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,30 +1,36 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
-
 import sys
+
 
 def solution():
     data = list(map(int, sys.stdin.buffer.read().split()))
     t = data[0]
-    idx = 1
 
-    cases = []
-
+    i = 1
     for _ in range(t):
-        n = data[idx]
-        idx += 1
+        n = data[i]
+        i += 1
 
-        applicants = []
+        result = []
         for _ in range(n):
-            doc = data[idx]
-            interview = data[idx + 1]
-            applicants.append((doc, interview))
-            idx += 2
+            grade1 = data[i]
+            grade2 = data[i + 1]
+            i += 2
+            result.append((grade1, grade2))
 
-        cases.append(applicants)
-        
-    result = 0
-    
-    print(result)
-    
-solution()
+        result.sort()
+
+        count = 1
+        best = result[0][1]
+
+        for i in range(1, n):
+            if result[i][1] < best:
+                count += 1
+                best = result[i][1]
+
+        print(count)
+
+
+if __name__ == "__main__":
+    solution()
