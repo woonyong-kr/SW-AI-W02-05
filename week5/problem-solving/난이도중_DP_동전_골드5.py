2f8acff4a16c58404e305212ac448b0957a71475
choi woo-nyong <woonyong.kr@gmail.com>
2026-04-01 10:49:24 +0900

    feat: 동전 DP 풀이를 추가

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,35 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+import sys
+
+
+def solution():
+    data = list(map(int, sys.stdin.buffer.read().split()))
+    t = data[0]
+    i = 1
+    answers = []
+
+    for _ in range(t):
+        n = data[i]
+        i += 1
+
+        coins = data[i : i + n]
+        i += n
+
+        m = data[i]
+        i += 1
+
+        dp = [0] * (m + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for money in range(coin, m + 1):
+                dp[money] += dp[money - coin]
+
+        answers.append(str(dp[m]))
+
+    print("\n".join(answers))
+
+
+if __name__ == "__main__":
+    solution()
