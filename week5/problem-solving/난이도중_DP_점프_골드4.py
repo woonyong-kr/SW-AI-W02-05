40dde29b69fba9a26445a8152fd3ba2a01ba9bf2
choi woo-nyong <woonyong.kr@gmail.com>
2026-04-01 10:50:00 +0900

    feat: 점프 DP 풀이를 추가

--- week5/problem-solving/난이도중_DP_점프_골드4.py
+++ week5/problem-solving/난이도중_DP_점프_골드4.py
@@ -1,2 +1,42 @@
 # DP - 점프 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2253
+import sys
+
+
+def solution():
+    data = list(map(int, sys.stdin.buffer.read().split()))
+    n = data[0]
+    m = data[1]
+
+    stones = set()
+    for i in range(2, 2 + m):
+        stones.add(data[i])
+
+    speed = int((2 * n) ** 0.5) + 2
+    inf = 10**9
+
+    dp = [[inf] * (speed + 1) for _ in range(n + 1)]
+    dp[1][0] = 0
+
+    for i in range(1, n + 1):
+        if i in stones:
+            continue
+
+        for v in range(speed):
+            if dp[i][v] == inf:
+                continue
+
+            for dv in (v - 1, v, v + 1):
+                if dv <= 0:
+                    continue
+
+                next_stone = i + dv
+                if next_stone <= n and next_stone not in stones:
+                    dp[next_stone][dv] = min(dp[next_stone][dv], dp[i][v] + 1)
+
+    result = min(dp[n])
+    print(result if result != inf else -1)
+
+
+if __name__ == "__main__":
+    solution()
